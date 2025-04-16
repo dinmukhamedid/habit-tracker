c4a1a4639f0718f47e194a4157a98e4814980de8
dinmukhamedid <dinmukhamedmaksot@gmail.com>
2025-04-16 11:42:25 +0500

    plusMigration

---
+++ config/migration.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strings"
+
+	"gorm.io/gorm"
+)
+
+func RunMigrations(db *gorm.DB) {
+	sqlBytes, err := ioutil.ReadFile("migration/1_init_tables.sql")
+	if err != nil {
+		log.Fatalf("Миграция файлын оқу қатесі: %v", err)
+	}
+
+	// SQL-ді бірнеше сұранысқа бөлу (егер бір файлда бірнеше сұраныс болса)
+	queries := strings.Split(string(sqlBytes), ";")
+	for _, query := range queries {
+		q := strings.TrimSpace(query)
+		if q != "" {
+			if err := db.Exec(q).Error; err != nil {
+				log.Fatalf("Миграция сұраныс қатесі: %v", err)
+			}
+		}
+	}
+
+	fmt.Println("🔧 SQL миграция сәтті орындалды.")
+}
